39f25b4a838497f7220fb5aaad693f0faaa8d3ba
Connor Zanin <cnnrznn@gmail.com>
2021-05-09 10:18:38 -0600

    Bencode parsers for tracker response

--- client/client.go
+++ client/client.go
@@ -2,9 +2,26 @@ package client
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/cnnrznn/torrent/file"
 )
+
+type TrackerResponse struct {
+	FailureReason  string `bencode:"failue reason"`
+	WarningMessage string `bencode:"warning message"`
+	Interval       int    `bencode:"interval"`
+	MinInterval    int    `bencode:"min interval"`
+	TrackerID      string `bencode:"tracker id"`
+	Complete       int    `bencode:"complete"`
+	Peers          []Peer `bencode:"peers"`
+}
+
+type Peer struct {
+	ID   string `bencode:"peer id"`
+	IP   string `bencode:"ip"`
+	port int    `bencode:"port"`
+}
 
 type Client struct {
 	torrent file.Torrent
@@ -17,5 +34,17 @@ func New(t file.Torrent) *Client {
 }
 
 func (c *Client) Run() {
-	fmt.Println("Run!")
+	tChan := make(chan TrackerResponse)
+	c.PingTracker(tChan)
+
+	for {
+		select {
+		case tu := <-tChan:
+			fmt.Println(tu)
+		}
+	}
+}
+
+func (c *Client) PingTracker(ch chan<- TrackerResponse) {
+	log.Println(c.torrent.Announcers)
 }
