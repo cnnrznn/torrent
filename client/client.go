96c114d00d06edd9cbbcd676df91c537e5f700b7
Connor Zanin <cnnrznn@gmail.com>
2021-05-09 11:43:08 -0600

    Reorganized, moved tracker source

--- client/client.go
+++ client/client.go
@@ -2,34 +2,21 @@ package client
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/cnnrznn/torrent/file"
+	"github.com/google/uuid"
 )
-
-type TrackerResponse struct {
-	FailureReason  string `bencode:"failue reason"`
-	WarningMessage string `bencode:"warning message"`
-	Interval       int    `bencode:"interval"`
-	MinInterval    int    `bencode:"min interval"`
-	TrackerID      string `bencode:"tracker id"`
-	Complete       int    `bencode:"complete"`
-	Peers          []Peer `bencode:"peers"`
-}
-
-type Peer struct {
-	ID   string `bencode:"peer id"`
-	IP   string `bencode:"ip"`
-	port int    `bencode:"port"`
-}
 
 type Client struct {
 	torrent file.Torrent
+	peerID  uuid.UUID
+	port    int
 }
 
 func New(t file.Torrent) *Client {
 	return &Client{
 		torrent: t,
+		peerID:  uuid.New(),
 	}
 }
 
@@ -43,8 +30,4 @@ func (c *Client) Run() {
 			fmt.Println(tu)
 		}
 	}
-}
-
-func (c *Client) PingTracker(ch chan<- TrackerResponse) {
-	log.Println(c.torrent.Announcers)
 }
